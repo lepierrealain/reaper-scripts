d29da1f8ae24d72ba41ab68cee95f932b6e6f8f8
lepierrealain <pa.lecroart@gmail.com>
2026-05-28 14:35:26 +0200

    Add split

--- Editing/PA_Split item under mouse cursor.lua
+++ Editing/PA_Split item under mouse cursor.lua
@@ -4,6 +4,7 @@
 
 local lib_path = ({ reaper.get_action_context() })[2]:match("^(.+[\\/])")
 dofile(lib_path .. "PA_lib_mouse.lua")
+dofile(lib_path .. "PA_lib_item.lua")
 
 local function main()
   local item, mouse_time = PA_GetItemUnderMouse()
@@ -12,8 +13,13 @@ local function main()
   local snap_enabled = reaper.GetToggleCommandState(1157) == 1
   local split_time = snap_enabled and reaper.SnapToGrid(0, mouse_time) or mouse_time
 
+  local grouped = PA_GetGroupedItemsAtSamePosition(item)
+
   reaper.Undo_BeginBlock()
   reaper.SplitMediaItem(item, split_time)
+  for _, gi in ipairs(grouped) do
+    reaper.SplitMediaItem(gi, split_time)
+  end
   reaper.UpdateArrange()
   reaper.Undo_EndBlock("Split item under mouse cursor", -1)
 end
