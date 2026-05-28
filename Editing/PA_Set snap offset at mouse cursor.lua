49acdb44d25669ff767b908bdb8ab783668a623b
lepierrealain <pa.lecroart@gmail.com>
2026-05-28 18:41:09 +0200

    Add snap offset

---
+++ Editing/PA_Set snap offset at mouse cursor.lua
@@ -0,0 +1,30 @@
+-- @description Set snap offset at mouse cursor
+-- @author lepierrealain
+-- @version 1.0
+
+local lib_path = ({ reaper.get_action_context() })[2]:match("^(.+[\\/])")
+dofile(lib_path .. "PA_lib_mouse.lua")
+dofile(lib_path .. "PA_lib_item.lua")
+
+local function main()
+  local item, mouse_time = PA_GetItemUnderMouse()
+  if not item then return end
+
+  local snap_enabled = reaper.GetToggleCommandState(1157) == 1
+  local snap_time = snap_enabled and reaper.SnapToGrid(0, mouse_time) or mouse_time
+
+  local item_start  = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+  local snap_offset = snap_time - item_start
+
+  local grouped = PA_GetGroupedItemsAtSamePosition(item)
+
+  reaper.Undo_BeginBlock()
+  reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", snap_offset)
+  for _, gi in ipairs(grouped) do
+    reaper.SetMediaItemInfo_Value(gi, "D_SNAPOFFSET", snap_offset)
+  end
+  reaper.UpdateArrange()
+  reaper.Undo_EndBlock("Set snap offset at mouse cursor", -1)
+end
+
+main()
